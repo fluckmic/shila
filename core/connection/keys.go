410d8a5963a7235f5cad0c065e0d2025615266a1
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-05-21 20:16:38 +0200

    Reworking

--- core/connection/keys.go
+++ core/connection/keys.go
@@ -3,8 +3,8 @@ package connection
 import "fmt"
 
 // Keys
-type Key string 		// (flow-key,flow-kind,flow-state)
+type Key string 		// (flow-key,flow-kind,current-flow-state)
 
 func (conn *Connection) Key() Key {
-	return Key(fmt.Sprint("(", conn.flow.Key(), ",", conn.kind, ",", conn.state, ")"))
+	return Key(fmt.Sprint("(", conn.flow.Key(), ",", conn.kind, ",", conn.state.current, ")"))
 }
