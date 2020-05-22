bc154511c02d432a29323cb7bb840bfbf590b3e7
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-05-22 10:28:46 +0200

    Reworking

--- core/router/router.go
+++ core/router/router.go
@@ -42,29 +42,19 @@ func (m Router) InsertFromIPAddressPortKey(key shila.IPAddressPortKey, srcAddr s
 func (m Router) UpdateFromSynAckMpCapable(p *shila.Packet) error {
 	if key, ok, err := mptcp.GetSenderKey(p.Payload); ok {
 		if err == nil {
-			if err := m.insertFromMPTCPEndpointKey(key, p.Flow.NetFlow); err != nil {
-				return Error(fmt.Sprint("Unable to insert MPTCP endpoint key. - ", err.Error()))
+			if token, err := mptcp.EndpointKeyToToken(key); err == nil {
+				return Error(fmt.Sprint("Unable to convert token from key. - ", err.Error()))
 			} else {
+				m.addressesFromToken[token] = p.Flow.NetFlow
 				return nil
 			}
 		} else {
 			return Error(fmt.Sprint("Error in fetching MPTCP endpoint key. - ", err.Error()))
 		}
 	} else {
+		// The packet does not necessarily contain the endpoint key (e.g. for a packet belonging to a subflow)
 		return nil
 	}
 }
 
-func (m Router) insertFromMPTCPEndpointKey(key mptcp.EndpointKey, flow shila.NetFlow) error {
-	if token, err := mptcp.EndpointKeyToToken(key); err != nil {
-		return Error(fmt.Sprint("Unable to convert from key to token. - ", err.Error()))
-	} else {
-		if _, ok := m.addressesFromToken[token]; ok {
-			return Error(fmt.Sprint("Entry already exists."))
-		} else {
-			m.addressesFromToken[token] = flow
-		}
-	}
-	return nil
-}
 
