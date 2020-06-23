d3c392ddf1decce99aad2a02723a8b3d3a393a72
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-23 13:07:52 +0200

    working

--- measurements/performance/connectionTester/runConnTestServer.sh
+++ measurements/performance/connectionTester/runConnTestServer.sh
@@ -2,8 +2,11 @@
 
 HOST_NAME=$(uname -n)
 
+BASE_DIR=$(dirname "$0")
+cd "$BASE_DIR"
+
 printf "Starting connection test server %s.\n" "$HOST_NAME"
 ../._connTest -name "$HOST_NAME" -p 27271
 
-return $!
+exit $!
 
