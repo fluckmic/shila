b8cf18f36e94da9bb0b318324630bedbe0befcc4
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-23 16:50:35 +0200

    working

--- measurements/performance/connectionTester/runConnTestClient.sh
+++ measurements/performance/connectionTester/runConnTestClient.sh
@@ -7,10 +7,12 @@ cd "$BASE_DIR"
 
 HOST_ID=$(cat ../_HOST_ID)
 
-CLIENT_ADDR=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271 20-ffaa:1:d89,[127.0.0.1]:27271 18-ffaa:1:d8a,[127.0.0.1]:27271)
+#CLIENT_ADDR=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271 20-ffaa:1:d89,[127.0.0.1]:27271 18-ffaa:1:d8a,[127.0.0.1]:27271)
+CLIENT_ADDR=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271)
+
 
 printf "Starting connection test client %s.\n" "$HOST_NAME"
-for i in {1..4}; do
+for i in {1..2}; do
   if [[ "$i" -ne "$HOST_ID" ]]; then
   .././_connTest -name "$HOST_NAME" -remote "${CLIENT_ADDR["$i"-1]}"
     if [[  $? -ne 0 ]]; then
