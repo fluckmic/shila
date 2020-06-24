fea8adb63f14f956db20565202a8f7238c25a592
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 10:25:14 +0200

    working

--- measurements/sessionScripts/checkForError.sh
+++ measurements/sessionScripts/checkForError.sh
@@ -3,7 +3,13 @@
 # Checks whether a certain session returned with an error.
 
 SESSION_NAME=$1
+PATH_TO_EXPERIMENT=$2
 
-ERROR_FILE=_"$SESSION_NAME"
+ERROR_FILE="$PATH_TO_EXPERIMENT""/_""$SESSION_NAME"
 
 if [[ -f "$ERROR_FILE" ]]; then
+  cat "$ERROR_FILE"
+  exit 1
+fi
+
+exit 0
