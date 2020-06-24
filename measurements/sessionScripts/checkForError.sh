6c197b7a29b185b6e7450d7e17ec95883e605ec9
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 14:03:06 +0200

    working

--- measurements/sessionScripts/checkForError.sh
+++ measurements/sessionScripts/checkForError.sh
@@ -8,9 +8,12 @@ PATH_TO_EXPERIMENT=$2
 ERROR_FILE="$PATH_TO_EXPERIMENT""/_""$SESSION_NAME"".err"
 
 if [[ -f "$ERROR_FILE" ]]; then
-  printf "Error : Failure in %s.\n" "$SESSION_NAME"".sh"
-  cat "$ERROR_FILE"
-  exit 1
+  ERROR_FILE_SIZE=$(du "$ERROR_FILE" | cut -f1)
+  if [[ "$ERROR_FILE_SIZE" -gt 0 ]]; then
+    printf "Error : Failure in %s.\n" "$SESSION_NAME"".sh"
+    cat "$ERROR_FILE"
+    exit 1
+  fi
 fi
 
 exit 0
