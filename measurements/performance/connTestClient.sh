6da9cf49ce32bcb528182e7405590020d96b111a
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 14:39:07 +0200

    working

--- measurements/performance/connTestClient.sh
+++ measurements/performance/connTestClient.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-HOST_NAME=$(uname -n)
-
 BASE_DIR=$(dirname "$0")
 cd "$BASE_DIR"
 
-HOST_ID=$(cat ../_HOST_ID)
+HOST_NAME=$(cat _hostName)
+HOST_ID=$(cat _hostId)
 
-#CLIENT_ADDR=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271 20-ffaa:1:d89,[127.0.0.1]:27271 18-ffaa:1:d8a,[127.0.0.1]:27271)
-CLIENT_ADDR=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271)
+# Load the addresses of the scion access points.
+ADDRESSES=$(cat scionApAddresses)
+OWN_ADDR=$(ADDRESSES["$HOST_ID"])
 
 
-printf "Starting connection test client %s.\n" "$HOST_NAME"
-for i in {1..2}; do
-  if [[ "$i" -ne "$HOST_ID" ]]; then
-  .././_connTest -name "$HOST_NAME" -remote "${CLIENT_ADDR["$i"-1]}"
-    if [[  $? -ne 0 ]]; then
-    continue
-    fi
+printf "Starting connection test client %s.\n" "$HOST_NAME" > _connTestClient.log
+
+done
+
+for ADDRESS in $(ADDRESSES[@]); do
+  if [[ "$ADDRESS" != "$OWN_ADDR" ]]; then
+    ./_connTest -name "$HOST_NAME" -remote "$ADDRESS" > _connTestClient.log 2> _connTestClient.error
   fi
 done
 
