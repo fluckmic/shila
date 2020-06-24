c9b46b859730152a0d0bab2ff96a183c3323c899
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 09:26:50 +0200

    working

--- measurements/sessionScripts/startSession.sh
+++ measurements/sessionScripts/startSession.sh
@@ -2,10 +2,4 @@
 
 # Starts a new detached tmux session.
 
-SESSION_NAME=$1
-SESSION_CMD=$2
-
-echo "$@"
-
-printf "Starting new session %s with command %s.\n" "$SESSION_NAME" "$SESSION_CMD"
-tmux new-session -d -s "$SESSION_NAME" "$SESSION_CMD"
+tmux new-session -d -s "$@"
