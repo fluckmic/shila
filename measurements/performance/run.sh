59ab502707d3398bda3754972fecf76e9882ce3f
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 09:05:08 +0200

    working

--- measurements/performance/run.sh
+++ measurements/performance/run.sh
@@ -2,31 +2,30 @@
 
 CLIENTS=(vm-1 vm-2)
 
+START_SESSION='bash ~/go/src/shila/measurements/sessionScripts/startSession.sh'
+
+
 ## First initialize all clients
 for CLIENT in "${CLIENTS[@]}"; do
-  ssh -tt scion@"$CLIENT" -q 'sudo bash ~/go/src/shila/measurements/performance/init.sh'
-  if [[ $? -ne 0 ]]; then
-    printf "Failed to initialize %s.\n" "$CLIENT"
-    exit 1
-  fi
+  ssh -tt scion@"$CLIENT" -q START_SESSION 'init' 'sudo bash ~/go/src/shila/measurements/performance/init.sh'
 done
 
 ## Then do a connection checks.
 #  Start the connection test servers.
-for SERVER in "${CLIENTS[@]}"; do
-  ssh -tt scion@"$SERVER" -q 'nohup sudo bash ~/go/src/shila/measurements/performance/connectionTester/runConnTestServer.sh >> ConnTestServer.log 2>&1 &'
-  if [[  $? -ne 0 ]]; then
-    printf "Failed to start connection test server %s.\n" "$SERVER"
-    exit 1
-  fi
-done
+#for SERVER in "${CLIENTS[@]}"; do
+#  ssh -tt scion@"$SERVER" -q 'nohup sudo bash ~/go/src/shila/measurements/performance/connectionTester/runConnTestServer.sh >> ConnTestServer.log 2>&1 &'
+#  if [[  $? -ne 0 ]]; then
+#    printf "Failed to start connection test server %s.\n" "$SERVER"
+#    exit 1
+#  fi
+#done
 
 #  Run the client side.
-for CLIENT in "${CLIENTS[@]}"; do
-  ssh -tt scion@"$CLIENT" -q 'sudo bash ~/go/src/shila/measurements/performance/connectionTester/runConnTestClient.sh'
-  if [[  $? -ne 0 ]]; then
-    printf "Connection test for client %s failed.\n" "$CLIENT"
-    exit 1
-  fi
-done
+#for CLIENT in "${CLIENTS[@]}"; do
+#  ssh -tt scion@"$CLIENT" -q 'sudo bash ~/go/src/shila/measurements/performance/connectionTester/runConnTestClient.sh'
+#  if [[  $? -ne 0 ]]; then
+#    printf "Connection test for client %s failed.\n" "$CLIENT"
+#    exit 1
+#  fi
+#done
 
