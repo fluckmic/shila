49fc998c50b2b8f0fba11ea76f7a8dc8b84c4e2b
Michael A. Flückiger <fmichael@student.ethz.ch>
2020-06-24 14:55:22 +0200

    working

--- measurements/performance/connTestClient.sh
+++ measurements/performance/connTestClient.sh
@@ -8,14 +8,13 @@ HOST_ID=$(cat _hostId)
 
 # Load the addresses of the scion access points.
 ADDRESSES=(17-ffaa:1:d87,[127.0.0.1]:27271 19-ffaa:1:d88,[127.0.0.1]:27271)
-OWN_ADDR=$(ADDRESSES["$HOST_ID"])
+OWN_ADDR="${ADDRESSES["$HOST_ID"]}"
 
 
 printf "Starting connection test client %s.\n" "$HOST_NAME" >> _connTestClient.log
 printf "Own addr: %s.\n" "$OWN_ADDR" >> _connTestClient.log
 
-for ADDRESS in $(ADDRESSES[@]); do
-  printf "Connection test to %s.\n" "$ADDRESS" > _connTestClient.log
+for ADDRESS in "${ADDRESSES[@]}"; do
   if [[ "$ADDRESS" != "$OWN_ADDR" ]]; then
     printf "Connection test to %s.\n" "$ADDRESS" > _connTestClient.log
     ./_connTest -name "$HOST_NAME" -remote "$ADDRESS" > _connTestClient.log 2> _connTestClient.error
