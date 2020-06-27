5988892e8891a00b8d5fda95cc429b67de8891e8
fluckmic <fluckmic@gmail.com>
2020-06-27 21:13:02 +0200

    preparing experiments

--- measurements/sessionScripts/terminateSession.sh
+++ measurements/sessionScripts/terminateSession.sh
@@ -6,12 +6,12 @@ echo "Enter terminate session."
 BASE_DIR=$(dirname "$0")
 cd "$BASE_DIR"
 
-echo "After base change"
-
 SESSION_NAME=$1
 
 ./isRunningSession.sh "$SESSION_NAME"
-if [[ $? -eq 0 ]]; then
+RETURN=$?
+echo "After isRunningSession"
+if [[ "$RETURN" -eq 0 ]]; then
   tmux kill-session -t "$SESSION_NAME" &>/dev/null
 fi
 
