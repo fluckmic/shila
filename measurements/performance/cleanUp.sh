0dc01702e518bd8b0819c6561a83c92aeb7b39b5
fluckmic <fluckmic@gmail.com>
2020-06-27 23:45:44 +0200

    preparing experiments

--- measurements/performance/cleanUp.sh
+++ measurements/performance/cleanUp.sh
@@ -6,6 +6,7 @@ BASE_DIR=$(dirname "$0")
 cd "$BASE_DIR"
 
 ## Reset everything to be ready for a new repetition.
+
 # Remove all builds as well.
 if [[ $1 -eq 1 ]]; then
   rm -f _*
@@ -14,6 +15,18 @@ fi
 # Remove all log and error files.
 rm -f _*.log
 rm -f _*.err
+
+# Kill all tmux sessions
+#sudo pkill -f tmux
+
+../sessionScripts/./terminateSession.sh shilaClientSide
+../sessionScripts/./terminateSession.sh shilaServerSide
+../sessionScripts/./terminateSession.sh iperfClientSide
+../sessionScripts/./terminateSession.sh iperfServerSide
+
+# Kill all shila and iperf instances
+#sudo pkill shila &>/dev/null
+#sudo pkill iperf &>/dev/null
 
 # Delete all namespaces
 bash ../../helper/netnsClear.sh
