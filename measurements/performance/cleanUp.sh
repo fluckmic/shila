49149f030d06483f97e51c40da42d3f6ab10d49d
fluckmic <fluckmic@gmail.com>
2020-06-27 15:36:47 +0200

    preparing experiments

--- measurements/performance/cleanUp.sh
+++ measurements/performance/cleanUp.sh
@@ -7,35 +7,23 @@ cd "$BASE_DIR"
 
 ## Reset everything to be ready for a new repetition.
 
-echo "Start cleanup."
-
 # Remove all builds as well.
 if [[ $1 -eq 1 ]]; then
   rm -f _*
 fi
 
-echo "1"
-
 # Remove all log and error files.
 rm -f _*.log
 rm -f _*.err
 
-echo "2"
-
 # Kill all tmux sessions
 tmux kill-server 2>/dev/null
-
-echo "3"
 
 # Kill all shila and iperf instances
 sudo pkill shila >&2>/dev/null
 sudo pkill iperf >&2>/dev/null
 
-echo "4"
-
 # Delete all namespaces
 bash ../../helper/netnsClear.sh
-
-echo "Done cleanup."
 
 exit 0
