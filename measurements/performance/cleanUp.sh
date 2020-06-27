e4af578d4499839ce9802bc185c7fbd6a9fb7df2
fluckmic <fluckmic@gmail.com>
2020-06-27 21:05:30 +0200

    preparing experiments

--- measurements/performance/cleanUp.sh
+++ measurements/performance/cleanUp.sh
@@ -19,10 +19,10 @@ rm -f _*.err
 # Kill all tmux sessions
 #sudo pkill -f tmux
 
-../sessionScripts/./terminateSession.sh "shilaClientSide"
-../sessionScripts/./terminateSession.sh "shilaServerSide"
-#../sessionScripts/./terminateSession.sh "iperfClientSide"
-../sessionScripts/./terminateSession.sh "iperfServerSide"
+../sessionScripts/./terminateSession.sh shilaClientSide
+../sessionScripts/./terminateSession.sh shilaServerSide
+../sessionScripts/./terminateSession.sh iperfClientSide
+../sessionScripts/./terminateSession.sh iperfServerSide
 
 # Kill all shila and iperf instances
 #sudo pkill shila &>/dev/null
