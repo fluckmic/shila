f6f26261497dc2870c7e2856b8825a20b4d5e628
fluckmic <fluckmic@gmail.com>
2020-06-27 21:59:22 +0200

    preparing experiments

--- measurements/sessionScripts/isRunningSession.sh
+++ measurements/sessionScripts/isRunningSession.sh
@@ -4,13 +4,13 @@
 
 SESSION_NAME=$1
 
-printf "Check if there is a running session %s?" "$SESSION_NAME"
+printf "Check if there is a running session %s?\n" "$SESSION_NAME"
 
 tmux has-session -t "$SESSION_NAME" &>/dev/null
 if [[ $= -eq 0 ]]; then
-  printf "There is a running session %s/n." "$SESSION_NAME"
+  printf "There is a running session %s\n." "$SESSION_NAME"
   exit 0
 else
-  printf "There is no running session %s/n." "$SESSION_NAME"
+  printf "There is no running session %s\n." "$SESSION_NAME"
   exit 1
 fi
