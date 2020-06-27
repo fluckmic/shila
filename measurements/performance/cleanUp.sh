4fefbf93e64fb62e2c1baa0f1c603ca18fc680ea
fluckmic <fluckmic@gmail.com>
2020-06-27 15:33:09 +0200

    preparing experiments

--- measurements/performance/cleanUp.sh
+++ measurements/performance/cleanUp.sh
@@ -14,16 +14,24 @@ if [[ $1 -eq 1 ]]; then
   rm -f _*
 fi
 
+echo "1"
+
 # Remove all log and error files.
 rm -f _*.log
 rm -f _*.err
 
+echo "2"
+
 # Kill all tmux sessions
 tmux kill-server 2>/dev/null
+
+echo "3"
 
 # Kill all shila and iperf instances
 sudo pkill shila
 sudo pkill iperf
+
+echo "4"
 
 # Delete all namespaces
 bash ../../helper/netnsClear.sh
