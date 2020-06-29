0a3f2351463a89a81b505eeff0b8e6a659164c1b
fluckmic <fluckmic@gmail.com>
2020-06-29 14:23:17 +0200

    preparing testing

--- testing/MultiInstance/send.sh
+++ testing/MultiInstance/send.sh
@@ -7,15 +7,20 @@ DST_ID=1
 DURATION=10
 INTERVAL=1
 
-
 mapfile -t PORTS < IperfListeningPorts.data
 mapfile -t CLIENTS < hostNames.data
 
 SRC_CLIENT=${CLIENTS["$SRC_ID"]}
 DST_CLIENT=${CLIENTS["$DST_ID"]}
 
-PORT=${PORTS["$REMOTE_ID"]}
+PORT=${PORTS["$DST_ID"]}
 
-printf "Send for %s seconds from %s to %s.\n" "$DURATION" "$SRC_CLIENT" "$DST_CLIENT"
+printf "Send for %s seconds from %s to %s (port %s).\n" "$DURATION" "$SRC_CLIENT" "$DST_CLIENT" "$PORT"
 
-#sudo ip netns exec shila-egress iperf -c "$ADDRESS" -p "$PORT" -t "$DURATION" -i "$INTERVAL"
+CMD="sudo ip netns exec shila-egress iperf3 -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"
+echo "$CMD"
+sshpass -f client.password ssh -tt scion@"$SRC_CLIENT" -q "$CMD"
+ if [[ $? -ne 0 ]]; then
+  printf "Failure : Unable to send.\n"
+  exit 1
+ fi
