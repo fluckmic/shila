06672c3b7e8f6a2c8d0cfad90442ee1df5b6c4f7
fluckmic <fluckmic@gmail.com>
2020-06-29 14:34:57 +0200

    preparing testing

--- testing/MultiInstance/send.sh
+++ testing/MultiInstance/send.sh
@@ -2,8 +2,8 @@
 
 ADDRESS="10.7.0.9"
 
-SRC_ID=0
-DST_ID=1
+SRC_ID=1
+DST_ID=0
 DURATION=10
 INTERVAL=1
 
@@ -17,7 +17,7 @@ PORT=${PORTS["$DST_ID"]}
 
 printf "Send for %s seconds from %s to %s (port %s).\n" "$DURATION" "$SRC_CLIENT" "$DST_CLIENT" "$PORT"
 
-CMD="sudo ip netns exec shila-egress iperf3 -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"
+CMD="sudo ip netns exec shila-egress iperf -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"
 echo "$CMD"
 sshpass -f client.password ssh -tt scion@"$SRC_CLIENT" -q "$CMD"
  if [[ $? -ne 0 ]]; then
