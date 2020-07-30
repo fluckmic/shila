774bdd807083fe2c4a1be436322f61dea9a8765e
fluckmic <fluckmic@gmail.com>
2020-07-30 07:14:39 +0200

    tsharking

--- measurements/performance/tsharkReceivingSide.sh
+++ measurements/performance/tsharkReceivingSide.sh
@@ -11,12 +11,12 @@ HOST_ID=$(cat _hostId)
 
 TSHARK_DUMP_FILENAME="_incomingTraffic.pcap"
 
-CAPTURE_FILTER="udp && dest port 50000"
+CAPTURE_FILTER="udp dst port 50000"
 
 #touch "$TSHARK_DUMP_FILENAME"
 #sudo chmod o=rw "$TSHARK_DUMP_FILENAME"
 
 printf "Starting tshark on the receiving side %s.\n" "$HOST_NAME" >> "$LOG_FILE"
-tshark -i eth0 -w "$TSHARK_DUMP_FILENAME" -F pcap -f "$CAPTURE_FILTER" > "$LOG_FILE" 2> "$ERR_FILE"
+tshark -i eth0 -w -f "$CAPTURE_FILTER" "$TSHARK_DUMP_FILENAME" -F pcap > "$LOG_FILE" 2> "$ERR_FILE"
 
 sleep 1
