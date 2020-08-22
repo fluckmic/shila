4f0e2ef67950df1e3a2c05123a65cc30e1390a62
fluckmic <fluckmic@gmail.com>
2020-08-22 18:11:38 +0200

    testing for report

--- testing/MultiInstanceLocal/runShila.sh
+++ testing/MultiInstanceLocal/runShila.sh
@@ -10,7 +10,15 @@ export PATH=$PATH:/usr/local/go/bin
 CLIENT_ID=$1
 CONFIG_FILE="config""$CLIENT_ID"".json"
 
-printf "Client %d - Starting shila..\n\n" "$CLIENT_ID"
+FOR_REPORT=$2
+
+if [[ $FOR_REPORT -eq 0 ]]; then
+	printf "Client %d - Starting shila..\n\n" "$CLIENT_ID"
+fi
+
+if [[ $FOR_REPORT -eq 1 ]]; then
+	printf "Host %d - Starting Shila..\n\n" $(($CLIENT_ID + 1))
+fi
 
 go build -o _shila ../../
 
--- testing/MultiInstanceLocal/send.sh
+++ testing/MultiInstanceLocal/send.sh
@@ -16,15 +16,32 @@ elif [[ $# -eq 3 ]]; then
   SRC_ID=$1
   DST_ID=$2
   DURATION=$3
+elif [[ $# -eq 4 ]]; then
+  SRC_ID=$1
+  DST_ID=$2
+  DURATION=$3
+  FOR_REPORT=$4
 fi
 
 mapfile -t PORTS < iperfListeningPorts.data
 PORT=${PORTS["$DST_ID"]}
 
+
+if [[ $FOR_REPORT -eq 1 ]]; then
+	PORT=27041
+fi
+
 NAMESPACE="shila-egress-""$SRC_ID"
 
 clear
-printf "Send for %s seconds from Client %s to Client %s (port %s).\n" "$DURATION" "$SRC_ID" "$DST_ID" "$PORT"
-CMD="iperf3 -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"" --get-server-output"
-echo "$CMD"
+if [[ $FOR_REPORT -eq 0 ]]; then
+	printf "Send for %s seconds from Client %s to Client %s (port %s).\n" "$DURATION" "$SRC_ID" "$DST_ID" "$PORT"
+	CMD="iperf3 -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"" --get-server-output"
+	echo "$CMD"
+fi
+if [[ $FOR_REPORT -eq 1 ]]; then 
+	printf "Host %d - Starting iPerf3..\n\n" $(($SRC_ID + 1))
+	CMD="iperf3 -c ""$ADDRESS"" -p ""$PORT"" -t ""$DURATION"" -i ""$INTERVAL"
+fi
+
 sudo ip netns exec "$NAMESPACE" $CMD
