389425e34a2b76cd1d63f49117b33887a4198c08
fluckmic <fluckmic@gmail.com>
2020-08-22 18:56:56 +0200

    experiment redone after renaming

--- testing/local/runIperfServer.sh
+++ testing/local/runIperfServer.sh
@@ -3,13 +3,21 @@
 clear
 
 CLIENT_ID=$1
+FOR_REPORT=$2
 
 mapfile -t PORTS < iperfListeningPorts.data
 
 PORT=${PORTS["$CLIENT_ID"]}
 NAMESPACE="shila-ingress-""$CLIENT_ID"
 
-printf "Client %d - Starting iperf3..\n\n" "$CLIENT_ID"
+if [[ $FOR_REPORT -eq 0 ]]; then
+	printf "Client %d - Starting iperf3..\n\n" "$CLIENT_ID"
+fi
+
+if [[ $FOR_REPORT -eq 1 ]]; then
+	printf "Host %d - Starting iPerf3..\n\n" $(($CLIENT_ID + 1))
+	PORT=27041
+fi
 
 sleep 15
 ip netns exec "$NAMESPACE" iperf3 -s -p "$PORT"
